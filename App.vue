af8f07b87f416a88fb5009601cfc49d2a673e3fe
Mohamed Reda BENAMMI <88676301+GhostSt94@users.noreply.github.com>
2022-04-23 02:41:13 +0000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <router-link to="/">Home</router-link> -->
+  <div class="container-fluid bg-light min-vh-100">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
